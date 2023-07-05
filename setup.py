0202db722fbb6ccce20fe3b2e21c23c859857f25
20938940+VHRanger@users.noreply.github.com <matthieu.ranger@outlook.com>
2023-07-05 16:47:27 -0400

    fixing long descr bull

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
 setup(
     name="YASE",
     version="1.0.0",
     packages=find_packages(),
-    description="Yet Another Sentence Embedding Library",
-
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     package_data={
         # If any package contains *.txt or *.rst files, include them:
         '': ['README.md', 'requirements.txt']
